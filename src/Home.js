d8964a8869d94ce20f71db036e03c703e279857a
KhanBora76 <khanbora76@gmail.com>
2022-02-27 13:19:29 +0000

    home.js-3 revised

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ import 'bulma/css/bulma.css';
 
 export const Home = () => (
     <>
-        <section class="hero">
+        <section class="hero is-primary">
             <div class="hero-body">
                 <p class="title is-size-1">
                     Harvest HST
@@ -16,38 +16,34 @@ export const Home = () => (
             </div>
         </section>
 
-        <div class="container is-fluid">
-
-            <div class="block">
-                <strong>HARVEST (aka H4RV3ST)</strong> helps maintain the sustainability of
-                water sources of the ecosystem. In today`s world we just
-                people realized the sources are not limitless and we as a
-                people are responsible for any other people and species as well.
-            </div>
-            <div class="block">
-                In the beginning, we were thinking about what we could do to save
-                the water for tomorrow, we realized saving the water is only possible
-                with care before educating the people.
-            </div>
-            <div class="block">
-                All we have been witnessed lots of harm and irrevocable demolitions
-                by some of the well-educated people in history. Before starting to
-                educate people there is something we need to do.
-            </div>
-            <div class="block">
-                Now we have learned that care is more important than anything. At
-                this point, we have been missioned to explain to the people what is
-                the benefit of saving water for tomorrow.
-            </div>
-            <div class="block">
-                We believe we can unite the people, we can explain what is pollution in
-                true meaning to protect our planet for future. And all we can work together
-                by hand in hand.
-            </div>
-            <div class="block">
-                Follow us, we know the path, we are the <strong>H4RV3S73R</strong>.
-            </div>
-
+        <div class="block">
+            <strong>HARVEST (aka H4RV3ST)</strong> helps maintain the sustainability of
+            water sources of the ecosystem. In today`s world we just
+            people realized the sources are not limitless and we as a
+            people are responsible for any other people and species as well.
+        </div>
+        <div class="block">
+            In the beginning, we were thinking about what we could do to save
+            the water for tomorrow, we realized saving the water is only possible
+            with care before educating the people.
+        </div>
+        <div class="block">
+            All we have been witnessed lots of harm and irrevocable demolitions
+            by some of the well-educated people in history. Before starting to
+            educate people there is something we need to do.
+        </div>
+        <div class="block">
+            Now we have learned that care is more important than anything. At
+            this point, we have been missioned to explain to the people what is
+            the benefit of saving water for tomorrow.
+        </div>
+        <div class="block">
+            We believe we can unite the people, we can explain what is pollution in
+            true meaning to protect our planet for future. And all we can work together
+            by hand in hand.
+        </div>
+        <div class="block">
+            Follow us, we know the path, we are the <strong>H4RV3S73R</strong>.
         </div>
 
         <hr />
@@ -87,7 +83,6 @@ export const Home = () => (
                 </p>
             </div>
         </footer>
-
 
     </>
 )
