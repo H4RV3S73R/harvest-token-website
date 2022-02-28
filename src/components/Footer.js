d76bb9bc1053410eaf3916bc140ea0472d4c4191
KhanBora76 <khanbora76@gmail.com>
2022-02-28 03:05:41 +0000

    Footer.js revised

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bulma/css/bulma.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 
 
@@ -9,6 +10,30 @@ export const Footer = () => (
     <footer className="footer">
 
       <div className="content has-text-centered">
+
+        <p>
+
+        <div>
+        <p><span><i className="fas fa-arrow-down" title="Follow us on"> Contact - Follow us on</i> <i className="fas fa-arrow-down" title="Follow us on"></i></span></p>
+
+          <h2>
+            <span className="icon-text has-text-info">
+
+              <span className="icon">
+                <a href="https://github.com/H4RV3S73R" target="_new" title="HARVEST on Github"><i className="fab fa-github" title="HARVEST on Github"></i></a>
+              </span>
+
+              <span className="icon">
+                <a href="https://www.twitter.com/H4RV3S73R" target="_new" title="HARVEST on Twitter"><i className="fab fa-twitter" title="HARVEST on Twitter"></i></a>
+              </span>
+
+            </span>
+
+          </h2>
+
+        </div>
+
+        </p>
 
         <p>
           <strong>Harvest HST Token </strong> on Avalanche Mainnet and <a href="https://snowtrace.io/address/0x96cb2c3E8f9Bcb22cA40B89AdF259c2642A6FF30#code" target="_new">
